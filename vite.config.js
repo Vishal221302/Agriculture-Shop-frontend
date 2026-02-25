a83f57a4ab4c4ed88f709fbb5fce3ce611af3331
Vishal <vishalpatel221302@gmail.com>
2026-02-25 11:37:29 +0530

    change api url

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 5173,
-        proxy: {
-            '/api': {
-                target: 'https://agriculture-shop-bakend.onrender.com',
-                changeOrigin: true
-            },
-            '/uploads': {
-                target: 'https://agriculture-shop-bakend.onrender.com',
-                changeOrigin: true
-            }
-        }
+  plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'https://agriculture-shop-bakend.onrender.com',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+      '/uploads': {
+        target: 'https://agriculture-shop-bakend.onrender.com',
+        changeOrigin: true,
+        secure: true
+      }
     }
+  }
 })
